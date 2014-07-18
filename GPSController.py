d39a4f949126b6ba2c4ad7f6d8905f12562fa697
Oklahomer <hagiwara.go@gmail.com>
2014-07-19 00:41:29 +0900

    Add some debug code for GPS module

--- GPSController.py
+++ GPSController.py
@@ -1,5 +1,7 @@
 import gps
 import threading
+import time
+import sys
 
 class GPSController(threading.Thread):
     def __init__(self):
@@ -26,3 +28,30 @@ class GPSController(threading.Thread):
     @property
     def utc(self):
         return self.gpsd.utc
+
+if __name__ == '__main__':
+    gpsController = GPSController()
+
+    try:
+        print('Locating...')
+
+        gpsController.start()
+
+        while (gpsController.isAlive()):
+            lat   = gpsController.fix.latitude
+            lon   = gpsController.fix.longitude
+            speed = gpsController.fix.speed
+            utc   = gpsController.utc
+
+            dataString = str(utc) + ',' + str(lat) + ',' + str(lon) + ',' + str(speed)
+            print(dataString)
+
+            time.sleep(3)
+    except KeyboardInterrupt:
+        print 'Cancelled'
+    
+    except:
+        print 'Unexpected error : ', sys.exc_info()[0], sys.exc_info()[1]
+    
+    finally:
+        gpsController.stopController()
