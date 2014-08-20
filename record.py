961e40c4f2a64b9bb149dfc75ddf580445dfb224
Oklahomer <hagiwara.go@gmail.com>
2014-08-20 13:15:31 +0000

    show preview on start

--- record.py
+++ record.py
@@ -12,6 +12,7 @@ os.putenv('SDL_MOUSEDRV'   , 'TSLIB'                 )
 os.putenv('SDL_MOUSEDEV'   , '/dev/input/touchscreen')
 
 odyssey = Odyssey()
+odyssey.show_preview()
 
 def switch_preview(channel):
     odyssey.switch_preview()
