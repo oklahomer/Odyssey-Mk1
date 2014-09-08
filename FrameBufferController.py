29096e1af31112f88acbc2b41ee57daca16a4b36
Oklahomer <hagiwara.go@gmail.com>
2014-09-08 13:59:03 +0000

    Add module to copy /dev/fb0 to /dev/fb1

---
+++ FrameBufferController.py
@@ -0,0 +1,43 @@
+import os
+import subprocess
+import threading
+import time
+
+class FrameBufferController(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+        self.cmd = os.path.dirname( os.path.abspath( __file__ )) + '/fbcp'
+
+    def run(self):
+        fbcp = subprocess.Popen(self.cmd)
+
+        self.running = True
+        while(self.running and fbcp.poll() is None):
+            time.sleep(0.5)
+
+        self.running = False
+        if fbcp.poll() == True: fbcp.kill()
+
+    def stopController(self):
+        self.running = False
+
+if __name__ == '__main__':
+    import sys
+
+    try:
+        controller = FrameBufferController()
+        controller.start()
+
+        print 'Copying framebuffer to /dev/fb1'
+
+        time.sleep(10)
+
+    except KeyboardInterrupt:
+        print 'Cancelled'
+
+    except:
+        print 'Unexpected error : ', sys.exc_info()[0], sys.exc_info()[1]
+
+    finally:
+        controller.stopController()
